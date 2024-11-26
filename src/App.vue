149850ed1a47bda78225d5c12db4616031de1d1e
kingston <kyawzinthet.developer@gmail.com>
2024-11-26 13:49:14 +0700

    fix: v1.2.2 build error and remove unnecessary packages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
 import Layout from '@/layout/index.vue'
-import { Analytics } from '@vercel/analytics'
-import { SpeedInsights } from '@vercel/speed-insights'
-
-
 </script>
 
 <template>
   <Layout>
     <router-view />
-    <Analytics />
-    <SpeedInsights />
   </Layout>
 </template>
