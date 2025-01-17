8759350cdb0314b7ff7b72868b209a9e483c3f58
kingston <kyawzinthet.developer@gmail.com>
2025-01-17 13:32:59 +0700

    remove vue dev tool

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import VueDevTools from "vite-plugin-vue-devtools";
+// import VueDevTools from "vite-plugin-vue-devtools";
 import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VueDevTools(),
+    // VueDevTools(),
     removeConsole(), // Remove console.log in production
   ],
   resolve: {
