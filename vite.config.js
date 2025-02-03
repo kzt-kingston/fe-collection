d72b9706a701a080b7f36c41a70f3baaa4351957
kingston <kyawzinthet.developer@gmail.com>
2025-02-03 14:00:51 +0700

    feat: transform into pwa

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // import VueDevTools from "vite-plugin-vue-devtools";
 import removeConsole from "vite-plugin-remove-console";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,27 @@ export default defineConfig({
     vue(),
     // VueDevTools(),
     removeConsole(), // Remove console.log in production
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Frontend Collection",
+        short_name: "FE Collection",
+        description: "A collection of frontend resources",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/logo-small.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/logo-medium.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
