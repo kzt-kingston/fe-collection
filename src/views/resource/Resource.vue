5d341a7a91d9dcbb16874adce0dc5452236c5c49
kingston <kyawzinthet.developer@gmail.com>
2025-02-27 13:53:05 +0700

    update: enhance ui by replacing buttons with tabs

--- src/views/resource/Resource.vue
+++ src/views/resource/Resource.vue
@@ -5,8 +5,8 @@ import { ref, onMounted, computed, watch } from 'vue'; // Add watch
 import resourceTitles from './data.json';
 import { getDictionary } from '@/locale/dict';
 import getData, {searchForData} from '@/util/getData';
-import { ElInput } from 'element-plus';
-import { Search } from 'lucide-vue-next';
+import { ElInput, ElNotification, ElTabs, ElTabPane } from 'element-plus'; // Add ElTabs imports
+import { Search, Heart, HeartOff } from 'lucide-vue-next';
 import WebsiteCard from './components/WebsiteCard.vue';
 import VideoCard from './components/VideoCard.vue';
 import PlaygroundCard from './components/PlaygroundCard.vue';
@@ -24,7 +24,7 @@ const hideResourceDetails = ref({
   playground: false
 })
 const searchResult = ref(null);
-const activeTab = ref('all');
+const activeTab = ref('all'); // Change activeTab to match ElTabs expected value type
 const inputData = ref(''); // Add missing ref
 
 // Create a debounced search function
@@ -42,9 +42,56 @@ watch(() => inputData.value, (newValue) => {
   debouncedSearch(newValue);
 });
 
+// Add bookmarks state
+const bookmarks = ref([]);
+
+// Add bookmark methods
+const saveBookMark = (id, title, url, category, resourceType) => {
+  const bookmark = { id, title, url, category, resourceType };
+  
+  // Check if bookmark already exists
+  const index = bookmarks.value.findIndex(
+    (b) => b.id === id && b.title === title && b.category === category
+  );
+
+  if (index !== -1) {
+    // Remove from bookmarks
+    bookmarks.value.splice(index, 1);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks.value));
+    ElNotification({
+      title: "Bookmark Removed",
+      message: "Bookmark removed successfully",
+      type: "info",
+      duration: 1000,
+    });
+  } else {
+    // Add to bookmarks
+    bookmarks.value.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks.value));
+    ElNotification({
+      title: "Bookmark Saved",
+      message: "Bookmark saved successfully",
+      type: "success",
+      duration: 1000,
+    });
+  }
+};
+
+// Reactive check for bookmarked status
+const isBookmarked = (id, title, category) => {
+  return bookmarks.value.some(
+    (bookmark) =>
+      bookmark.id === id &&
+      bookmark.title === title &&
+      bookmark.category === category
+  );
+};
+
 onMounted(() => {
   const lang = localStorage.getItem('lang') || 'en';
   dict.value = getDictionary(lang);
+  // Load bookmarks from localStorage
+  bookmarks.value = JSON.parse(localStorage.getItem("bookmarks")) || [];
   // Perform initial search if there's any input value
   if (inputData.value) {
     debouncedSearch(inputData.value);
@@ -127,76 +174,109 @@ const emits = defineEmits(['openNewTab']);
 
     <!-- search result -->
     <div v-if="searchResult" class="my-5">
-      <div class="container mx-auto">
+      <div class="px-4 py-4">
         <h1 class="text-3xl font-bold mb-6">{{ dict.search_result }}</h1>
 
+        <!-- Replace buttons with ElTabs -->
         <div class="mb-8">
-          <div class="flex gap-2 border-b pb-2">
-            <button 
-              class="px-4 py-2 border rounded-md flex items-center gap-2" 
-              :class="{ 'bg-gray-200': activeTab === 'all' }"
-              @click="activeTab = 'all'"
-            >
-              All
-              <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
-                {{ totalCount }}
-              </span>
-            </button>
-            <button 
-              v-if="hasWebsites" 
-              class="px-4 py-2 border rounded-md flex items-center gap-2"
-              :class="{ 'bg-gray-200': activeTab === 'websites' }" 
-              @click="activeTab = 'websites'"
-            >
-              Websites
-              <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
-                {{ websitesCount }}
-              </span>
-            </button>
-            <button 
-              v-if="hasVideos" 
-              class="px-4 py-2 border rounded-md flex items-center gap-2"
-              :class="{ 'bg-gray-200': activeTab === 'videos' }" 
-              @click="activeTab = 'videos'"
-            >
-              Videos
-              <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
-                {{ videosCount }}
-              </span>
-            </button>
-            <button 
-              v-if="hasPlayground" 
-              class="px-4 py-2 border rounded-md flex items-center gap-2"
-              :class="{ 'bg-gray-200': activeTab === 'playground' }"
-              @click="activeTab = 'playground'"
-            >
-              Playground
-              <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
-                {{ playgroundCount }}
-              </span>
-            </button>
-          </div>
+          <el-tabs v-model="activeTab" class="demo-tabs">
+            <el-tab-pane label="All" name="all">
+              <template #label>
+                <span class="flex items-center gap-2">
+                  All
+                  <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
+                    {{ totalCount }}
+                  </span>
+                </span>
+              </template>
+            </el-tab-pane>
+            
+            <el-tab-pane v-if="hasWebsites" name="websites">
+              <template #label>
+                <span class="flex items-center gap-2">
+                  Websites
+                  <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
+                    {{ websitesCount }}
+                  </span>
+                </span>
+              </template>
+            </el-tab-pane>
+
+            <el-tab-pane v-if="hasVideos" name="videos">
+              <template #label>
+                <span class="flex items-center gap-2">
+                  Videos
+                  <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
+                    {{ videosCount }}
+                  </span>
+                </span>
+              </template>
+            </el-tab-pane>
+
+            <el-tab-pane v-if="hasPlayground" name="playground">
+              <template #label>
+                <span class="flex items-center gap-2">
+                  Playground
+                  <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-cyan-500 rounded-full">
+                    {{ playgroundCount }}
+                  </span>
+                </span>
+              </template>
+            </el-tab-pane>
+          </el-tabs>
         </div>
 
         <div v-if="hasWebsites && (activeTab === 'websites' || activeTab === 'all')" class="mb-5">
           <h2 class="text-2xl font-semibold mb-4">Websites</h2>
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <WebsiteCard v-for="website in searchResult.websites" :key="website.id" :website="website" />
+            <div v-for="website in searchResult.websites" :key="website.id" class="relative">
+              <component 
+                :is="isBookmarked(website.id, website.title, selectedTitle) ? HeartOff : Heart"
+                @click="() => saveBookMark(website.id, website.title, website.url, selectedTitle, 'websites')"
+                class="hover:text-red-500 text-gray-400 cursor-pointer absolute top-2 right-2 z-10"
+                size="20"
+                :class="{
+                  'text-red-500': isBookmarked(website.id, website.title, selectedTitle),
+                }"
+              />
+              <WebsiteCard :website="website" />
+            </div>
           </div>
         </div>
 
         <div v-if="hasVideos && (activeTab === 'videos' || activeTab === 'all')" class="mb-5">
           <h2 class="text-2xl font-semibold mb-4">Videos</h2>
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <VideoCard v-for="video in searchResult.videos" :key="video.id" :video="video" />
+            <div v-for="video in searchResult.videos" :key="video.id" class="relative">
+              <component 
+                :is="isBookmarked(video.id, video.title, selectedTitle) ? HeartOff : Heart"
+                @click="() => saveBookMark(video.id, video.title, `https://www.youtube.com/watch?v=${video.id}`, selectedTitle, 'videos')"
+                class="hover:text-red-500 text-gray-400 cursor-pointer absolute top-2 right-2 z-10"
+                size="20"
+                :class="{
+                  'text-red-500': isBookmarked(video.id, video.title, selectedTitle),
+                }"
+              />
+              <VideoCard :video="video" />
+            </div>
           </div>
         </div>
 
         <div v-if="hasPlayground && (activeTab === 'playground' || activeTab === 'all')" class="mb-5">
           <h2 class="text-2xl font-semibold mb-4">Playground</h2>
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <PlaygroundCard v-for="playground in searchResult.playground" :key="playground.id"
-              :playground="playground" />
+            <div v-for="playground in searchResult.playground" :key="playground.id" class="relative">
+              <component 
+                :is="isBookmarked(playground.id, playground.title, selectedTitle) ? HeartOff : Heart"
+                @click="() => saveBookMark(playground.id, playground.title, playground.url, selectedTitle, 'playground')"
+                class="hover:text-red-500 text-gray-400 cursor-pointer absolute top-2 right-2 z-10"
+                size="20"
+                :class="{
+                  'text-red-500': isBookmarked(playground.id, playground.title, selectedTitle),
+                }"
+              />
+              <PlaygroundCard :playground="playground" />
+            </div>
           </div>
         </div>
       </div>
@@ -303,5 +383,25 @@ const emits = defineEmits(['openNewTab']);
   background-color: rgb(6, 182, 212);
   transform: scale(1.1);
   transition: transform 0.5s;
+}
+
+:deep(.el-tabs__nav-wrap::after) {
+  height: 1px;
+}
+
+:deep(.el-tabs__item) {
+  font-size: 14px;
+  
+  &.is-active {
+    color: rgb(6, 182, 212);
+  }
+  
+  &:hover {
+    color: rgb(6, 182, 212);
+  }
+}
+
+:deep(.el-tabs__active-bar) {
+  background-color: rgb(6, 182, 212);
 }
 </style>
